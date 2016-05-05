92956e65ff502672e381ab9bda059fb335a88638
JennyLuciaVg <jelusita11@gmail.com>
2016-05-05 00:14:21 -0500

    jquery modify

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+var $data = $('#data');
+
 var myFirebaseRef = new Firebase("https://crackling-fire-4441.firebaseio.com/");
-myFirebaseRef.child("message").on("value", function(snapshot) {
-  alert(snapshot.val()); 
+myFirebaseRef.child("message").on("child_added", function(snapshot) {
+  $data.text(+$data.text() + 1);
+  //$data.append(snapshot.val().field);
 });
